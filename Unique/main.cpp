afb4af9a62fcdcd1383a4066738fbdd9fd2c58f4
yumin <yumin.dima@gmail.com>
2024-04-02 20:25:56 +0300

    Unique DONE

---
+++ Unique/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+
+	for (int i = 0; i < n; i++) {
+		bool unique;
+		do
+		{
+			arr[i] = rand() % n;
+			unique = true;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					unique = false;
+					break;
+				}
+			}
+		} while (!unique);
+	}
+}
